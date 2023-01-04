3a89429f77fd0b1ad553dcffd3bb2fa7fc74a9d2
c-livi <121881563+c-livi@users.noreply.github.com>
2023-01-04 11:24:14 -0500

    Update obfuscator-main.lua

--- obfuscator-main.lua
+++ obfuscator-main.lua
@@ -1,25 +1,24 @@
+local obfs = {}
 
-local obfs={}
-
-local function Read(str,sep)
-	if sep == nil then
-      		sep = "%s"
-   	end
-   	local t={}
-   	for _str in string.gmatch(str, "([^"..sep.."]+)") do
-      		table.insert(t, _str)
-   	end
-   	return t;
+local function Read(str, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local t = {}
+  for _str in string.gmatch(str, "([^"..sep.."]+)") do
+    table.insert(t, _str)
+  end
+  return t
 end
 
 local function Rdm(len)
-	local _s = ""
-	for i = 1, len do
-		_a=string.char(math.random(97, 122))
-		if math.random(2)==1 then _a=_a:upper() end
-		_s = _s .. _a
-	end
-	return _s;
+  local _s = ""
+  for i = 1, len do
+    _a = string.char(math.random(97, 122))
+    if math.random(2) == 1 then _a = _a:upper() end
+    _s = _s .. _a
+  end
+  return _s
 end
 
 local function split(str, delimiter)
@@ -38,31 +37,33 @@ local function shuffle(array)
 end
 
 function obfs.obfuscateVariableNames(s)
-	if type(s)~='string' then return nil end;
-	local _vars=0
-	local t=Read(s,'=')
-	for _i,_ in ipairs(t) do
-		local _variable=t[_i]
-		if _variable:match('local') then
-			_variable=Read(_variable,' ')[#Read(_variable,' ')]
-			if not _variable:match('%.') then
-				_vars=_vars+1
-				s=s:gsub(_variable,Rdm(5).._vars)
-			end
-		end
-	end
-	return s;
+  local lines = split(script, "\n")
+
+  for i, line in ipairs(lines) do
+    line = string.gsub(line, "%b()", function(match)
+      local tokens = split(match, " ")
+      for j, token in ipairs(tokens) do
+        if token ~= "local" and token ~= "function" and not tonumber(token) then
+          tokens[j] = generate_random_string()
+        end
+      end
+      return table.concat(tokens, " ")
+    end)
+    lines[i] = line
+  end
+  local obfuscated_script = table.concat(lines, "\n")
+  return obfuscated_script
 end
 
 function obfs.obfuscateFunctions(s)
-	return string.gsub(s, "function", Rdm(30));
+  return string.gsub(s, "function", Rdm(30))
 end
 
 function obfs.obfuscateControlFlow(s)
-  	local lines = split(s, "\n")
-  	shuffle(lines)
-  	local obfuscated_script = table.concat(lines, "\n")
-  	return obfuscated_script;
+  local lines = split(s, "\n")
+  shuffle(lines)
+  local obfuscated_script = table.concat(lines, "\n")
+  return obfuscated_script
 end
 
-return obfs;
+return obfs
