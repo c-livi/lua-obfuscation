6ef8add634bda65ebc653716726b00c7b5409c10
c-livi <121881563+c-livi@users.noreply.github.com>
2023-01-04 08:34:18 -0500

    Update obfuscator-main.lua

--- obfuscator-main.lua
+++ obfuscator-main.lua
@@ -1,7 +1,31 @@
-local self = {}
+local obfs={}
 
-local function grabRestrictedFunctions()
-
+function obfs.changeVariableNames(s)
+	if type(s)~='string' then return nil end;
+	local _vars=0
+	local function Read(str,sep)
+		local t={}
+        for _str in string.gmatch(str, "([^"..sep.."]+)") do
+                table.insert(t, _str)
+        end
+		return t;
+	end
+	local t=Read(s,'=')
+	for _i,_ in ipairs(t) do
+		local _variable=t[_i]
+		if _variable:match('local') then
+			_variable=Read(_variable,' ')[#Read(_variable,' ')]
+			if not _variable:match('%.') then
+				_vars=_vars+1
+				s=s:gsub(_variable,'__V_'.._vars)
+			end
+		end
+	end
+	return s;
 end
 
-function self:obfuscate(string)
+function obfs.changeValueNames(s)
+	
+end
+
+return obfs;
